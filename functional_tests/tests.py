92436efab8ce1ea0ff309486e97ede8315829410
olvt01 <34155609+olvt01@users.noreply.github.com>
2018-07-02 03:42:01 +0900

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,18 +1,20 @@
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
 import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
@@ -121,14 +123,14 @@ class NewVisitorTest(LiveServerTestCase):
     def test_layout_and_styling(self):
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024,768)
+        self.browser.set_window_size(1014, 768)
 
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=20
         )
 
         # She starts a new list and sees the input is nicely
@@ -140,7 +142,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=20
         )
 
 
--- lists/urls.py
+++ lists/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
     path('new', views.new_list, name='new_list'),
     path('<int:list_id>/', views.view_list, name='view_list'),
     path('<int:list_id>/add_item', views.add_item, name='add_item'),
