ffb96fafc21d17fc160da459ea2c5bea673782d3
olvt01 <34155609+olvt01@users.noreply.github.com>
2018-07-01 15:03:00 +0900

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -9,25 +9,11 @@ from .models import Item
 
 class HomePageTest(TestCase):
 
-    # def test_root_url_resolves_to_home_page_view(self):
-    #     found = resolve('/')
-    #     self.assertEqual(found.func, home_page)
-    #
-    #
-    # def test_home_page_returns_correct_html(self):
-    #     request = HttpRequest()
-    #     response = home_page(request)
-    #     html = response.content.decode('UTF8')
-    #     expected_html = render_to_string('lists/home.html')
-    #     self.assertEqual(html, expected_html)
-
-    # These two long-winded tests into one: test_uses_home_templates
-
     def test_uses_home_templates(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
 
-    def text_can_save_a_POST_request(self):
+    def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
@@ -37,20 +23,11 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -71,3 +48,19 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class LiveViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.clinet.get('/lists/the-only-list-in-the-world')
+        self.assertTemplateUsed(response, 'lists/list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
