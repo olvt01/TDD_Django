6380da1b5ee80041c7778223980ab0ecdb8af732
olvt01 <34155609+olvt01@users.noreply.github.com>
2018-07-12 18:06:17 +0900

    new form for list items

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,6 +6,7 @@ from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 class HomePageTest(TestCase):
@@ -13,6 +14,10 @@ class HomePageTest(TestCase):
     def test_uses_home_templates(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class NewListTest(TestCase):
