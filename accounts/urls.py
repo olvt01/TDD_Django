5f3aa64717c7aa961fe43afac3c71c4aef3ab797
olvt01 <34155609+olvt01@users.noreply.github.com>
2018-08-27 03:14:13 +0900

    test_my_lists: precreate sessions, move login checks into base

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
-
+from django.contrib.auth.views import logout
 from accounts import views
+
 
 urlpatterns = [
     path('send_login_email', views.send_login_email, name='send_login_email'),
     path('login', views.login, name='login'),
+    path('logout', logout, {'next_page': '/'}, name='logout'),
 ]
---
+++ functional_tests/test_my_lists.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
+from django.contrib.sessions.backends.db import SessionStore
+from .base import FunctionalTest
+User = get_user_model()
+
+
+class MyListsTest(FunctionalTest):
+
+    def create_pre_authenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+
+        ## create a session object in the database.
+        ## The session key is the primary key of the user object
+        ## (which is actually the user’s email address).
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+        ## to set a cookie we need to first visit the domain.
+        ## 404 pages Load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+
+            ## We then add a cookie to the browser that matches the session
+            ## on the server—​on our next visit to the site,
+            ## the server should recognise us as a logged-in user.
+            value=session.session_key,
+            path='/',
+        ))
+
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        email = 'edith@example.com'
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_in(email)
+
+        # Edith is a logged in user
+        self.create_pre_authenticated_session(email)
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_in(email)
