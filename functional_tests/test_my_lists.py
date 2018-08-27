613aa8d57725b9eac2a2c495407690a983222cfd
olvt01 <34155609+olvt01@users.noreply.github.com>
2018-08-27 21:01:51 +0900

    add test_my_lists & refactor base.py to de-duplicate codes with using decorators

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -33,7 +33,7 @@ class MyListsTest(FunctionalTest):
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         email = 'edith@example.com'
         self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_in(email)
+        self.wait_to_be_logged_out(email)
 
         # Edith is a logged in user
         self.create_pre_authenticated_session(email)
